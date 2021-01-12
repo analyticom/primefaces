f105ccc3438d8ed9d023c5daa37d2cb6d33ec172
mbaricevic <marko.baricevic@analyticom.de>
2021-01-12 12:12:34 +0100

    Merge remote-tracking branch 'upstream/master' into master-9.0-patch-12 FIX

--- src/main/java/org/primefaces/component/datatable/feature/FilterFeature.java
+++ src/main/java/org/primefaces/component/datatable/feature/FilterFeature.java
@@ -53,6 +53,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class FilterFeature implements DataTableFeature {
 
